9d0b86c0f7744080a12a3db126ea09163fb48f5e
amuii <ricloful@gmail.com>
2015-02-27 19:35:45 +0100

    Segunda subida

--- app/src/main/java/com/ricloful/amuii/myaiot/MainActivity.java
+++ app/src/main/java/com/ricloful/amuii/myaiot/MainActivity.java
@@ -4,14 +4,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
+import android.widget.ScrollView;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
+
+    synchronized void imprimir(final String cad) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                TextView ll = (TextView) findViewById(R.id.TextViewEstado);
+                ll.append(cad);
+                ScrollView ll1 = (ScrollView) findViewById(R.id.scrollView);
+                ll1.fullScroll(View.FOCUS_DOWN);
+            }});
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hilo de registro
+        new Thread(new Runnable() {
+            public void run() {
+                imprimir("Hilo de Registro");
+                // inicio flowcloud
+                // registro
+                // obtener nombre
+            }}
+        ).start();
+
+        //hilo de prueba
+        new Thread(new Runnable() {
+            public void run() {
+                imprimirl("Hilo de Prueba");
+                while (true){
+                    try{
+                        Thread.sleep(1000);
+
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                // registro
+                // obtener nombre
+            }}
+        ).start();
+    }
+
+    void imprimirl (String cad){
+        imprimir(cad+"\r\n");
     }
 
 
